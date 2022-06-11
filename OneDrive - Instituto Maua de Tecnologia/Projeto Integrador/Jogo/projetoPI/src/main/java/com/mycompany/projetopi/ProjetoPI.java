5380bcf1c6d0ecf693d4c2592c634023ef05614c
Briqz23 <danielbriquez2@gmail.com>
2022-06-11 00:20:10 -0300

    Signed-off-by: speedo2333 <93158678+speedo2333@users.noreply.github.com> Signed-off-by: Briqz23 <danielbriquez2@gmail.com>

--- OneDrive - Instituto Maua de Tecnologia/Projeto Integrador/Jogo/projetoPI/src/main/java/com/mycompany/projetopi/ProjetoPI.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.projetopi;
-
-/** Aqui rodaremos o jogo
-public class ProjetoPI {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
